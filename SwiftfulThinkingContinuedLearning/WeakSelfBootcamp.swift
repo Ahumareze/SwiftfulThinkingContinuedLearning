30a99155d0e4a142d62fb1aab97e275d5664bd3f
Ahumareze <ahumarezeifeanyi001@gmail.com>
2023-10-16 22:38:41 +0100

    learnt weak self

---
+++ SwiftfulThinkingContinuedLearning/WeakSelfBootcamp.swift
@@ -0,0 +1,81 @@
+//
+//  WeakSelfBootcamp.swift
+//  SwiftfulThinkingContinuedLearning
+//
+//  Created by Ifeanyi Ahumareze on 16/10/2023.
+//
+
+import SwiftUI
+
+struct WeakSelfBootcamp: View {
+    
+    @AppStorage("count") var count: Int?
+    
+    init(){
+        count = 0
+    }
+    
+    var body: some View {
+        NavigationView {
+            NavigationLink("Navigate", destination: WeakSelfSecondScreen())
+            .navigationTitle("Screen 1")
+        }
+        .overlay (
+            Text("\(count ?? 0)")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.green.cornerRadius(10))
+                .padding(.horizontal)
+                .padding(.top, 20)
+            
+            , alignment: .topTrailing
+        )
+    }
+}
+
+struct WeakSelfSecondScreen: View{
+    
+    @StateObject var vm = weakSelfSecondScreenViewmodel()
+    
+    var body: some View{
+        VStack {
+            Text("Second view")
+                .font(.largeTitle)
+            .foregroundColor(Color.red)
+            
+            if let data = vm.data{
+                Text(data)
+            }
+        }
+    }
+}
+
+class weakSelfSecondScreenViewmodel: ObservableObject{
+    @Published var data: String? = nil
+    
+    init(){
+        print("INITIALIZE NOW")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount + 1, forKey: "count")
+        getData()
+    }
+    
+    deinit {
+        print("DEINITIALIZE NOW")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount - 1, forKey: "count")
+    }
+    
+    func getData(){
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 500){ [weak self] in
+            self?.data = "NEW DATA!!!"
+        }
+    }
+}
+
+struct WeakSelfBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        WeakSelfBootcamp()
+    }
+}
